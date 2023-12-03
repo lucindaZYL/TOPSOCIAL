9a4a2455ca118652a3a47727c5a70cf5e83b28d4
lucindaZYL <147612715+lucindaZYL@users.noreply.github.com>
2023-12-03 17:16:30 +1100

    updated

---
+++ Backend/controllers/messageController.js
@@ -0,0 +1,21 @@
+const axios =require('axios');
+
+const getMessage =async function(req,res){
+    try{
+const url ='http://localhost:8000/message';
+const response =await axios.get(url);
+
+res.status(200).json({
+msg:'get message succeed',
+data:{message:message.data
+
+}
+})
+    } catch(error){console.error('error',error);
+
+}
+}
+
+
+module.exports ={getMessage}
+
